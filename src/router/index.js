f5ad137071e1ccb04be994d142899576f6f86ab8
ajp84 <prater.anthony.j@gmail.com>
2024-03-10 19:33:00 -0700

    add update paths for page routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,12 @@ import AmateurTraining from '../components/programs/AmateurTraining.vue'
 import YouthTraining from '../components/programs/YouthTraining.vue'
 import ProfessionalTraining from '../components/programs/ProfessionalTraining.vue'
 import TeamCoaches from '@/views/TeamCoaches.vue'
+import ApplyForm from '@/views/ApplyForm.vue'
+import ReferencesList from '@views/ReferencesList.vue'
+import ContactView from '@/views/ContactView.vue'
+import TrainingGrounds from '@/views/TrainingGrounds.vue'
+import GalleryView from '@/views/GalleryView.vue'
+import LatestNews from '@/views/LatestNews.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,103 +19,103 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeView
     },
     {
       path: '/about',
       name: 'about-us',
       props: true,
-      component: AboutUs,
+      component: AboutUs
     },
     {
       path: '/coaches/',
       name: 'team-coaches',
       props: true,
-      component: TeamCoaches, 
+      component: TeamCoaches,
       children: [
         {
           // UserProfile will be rendered inside User's <router-view>
           // when /user/:id/profile is matched
           path: 'youth',
           name: 'youth',
-          component: YouthTraining,
+          component: YouthTraining
         },
         {
           // UserPosts will be rendered inside User's <router-view>
           // when /user/:id/posts is matched
           path: 'amateur',
           name: 'amateur',
-          component: AmateurTraining,
+          component: AmateurTraining
         },
         {
           // UserPosts will be rendered inside User's <router-view>
           // when /user/:id/posts is matched
           path: 'professional',
           name: 'professional',
-          component: ProfessionalTraining,
-        },
-      ],
+          component: ProfessionalTraining
+        }
+      ]
     },
     {
       path: '/training/',
       name: 'training-programs',
       props: true,
-      component: TrainingPrograms, 
+      component: TrainingPrograms,
       children: [
         {
           // UserProfile will be rendered inside User's <router-view>
           // when /user/:id/profile is matched
           path: 'youth',
           name: 'youth',
-          component: YouthTraining,
+          component: YouthTraining
         },
         {
           // UserPosts will be rendered inside User's <router-view>
           // when /user/:id/posts is matched
           path: 'amateur',
           name: 'amateur',
-          component: AmateurTraining,
+          component: AmateurTraining
         },
         {
           // UserPosts will be rendered inside User's <router-view>
           // when /user/:id/posts is matched
           path: 'professional',
           name: 'professional',
-          component: ProfessionalTraining,
-        },
-      ],
+          component: ProfessionalTraining
+        }
+      ]
     },
     {
       path: '/apply',
       name: 'apply-form',
-      component: () => import('../views/ApplyForm.vue'),
+      component: ApplyForm
     },
     {
       path: '/references',
       name: 'references',
-      component: () => import('@/views/ReferencesList.vue'),
+      component: ReferencesList
     },
     {
       path: '/contact',
       name: 'contact',
-      component: () => import('@/views/ContactView.vue'),
+      component: ContactView
     },
     {
       path: '/training_grounds',
       name: 'training-grounds',
-      component: () => import('@/views/TrainingGrounds.vue'),
+      component: TrainingGrounds
     },
     {
       path: '/photo_gallery',
       name: 'photo-gallery',
-      component: () => import('@/views/GalleryView.vue'),
+      component: GalleryView
     },
     {
       path: '/news',
       name: 'latest-news',
-      component: () => import('@/views/LatestNews.vue'),
-    },
-  ],
+      component: LatestNews
+    }
+  ]
 })
 
 export default router
