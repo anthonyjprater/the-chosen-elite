ec34b4178c3fbe2cb1ac4882fa279b8f304ab3d3
ajp1984 <prater.anthony.j@gmail.com>
2024-04-30 15:34:12 -0700

    Update HeadCoach semantics.vue

--- src/components/HeadCoach.vue
+++ src/components/HeadCoach.vue
@@ -11,17 +11,17 @@ function getImageUrl(name, ext) {
         <div>
             <article>
                 <h2>Head coach - Timothy Prater </h2>
-                <p class="headcoach-details">Meet Timothy Prater, a basketball virtuoso hailing from the heart of Louisiana. From a tender age, Timothy's love affair with basketball ignited a flame that would shape his destiny. Fuelled by an unwavering passion and an unyielding work ethic, he embarked on a journey marked by dedication and innovation.
+                <p class="headcoach-details">Meet Timothy Prater, a basketball virtuoso hailing from the heart of Louisiana. From a tender age, Timothy's love affair with basketball ignited a flame that would shape his destiny. Fuelled by an unwavering passion and an unyielding work ethic, he embarked on a journey marked by dedication and innovation.</p>
 
-Timothy's basketball journey began as a varsity player in 8th grade, setting the stage for a remarkable career trajectory. Yet, his story transcends mere accolades; it's a testament to his commitment to the game he loves. After years of honing his skills on the court, he felt a calling to give back, to impart his knowledge and expertise to the next generation of hoop dreamers.
+<p>Timothy's basketball journey began as a varsity player in 8th grade, setting the stage for a remarkable career trajectory. Yet, his story transcends mere accolades; it's a testament to his commitment to the game he loves. After years of honing his skills on the court, he felt a calling to give back, to impart his knowledge and expertise to the next generation of hoop dreamers.</p>
 
-His foray into coaching was a natural evolution, driven by a desire to nurture talent and cultivate greatness. As he trained a group of local high school kids, he witnessed their transformation, their ascent to collegiate and even professional ranks. It was a realization of the profound impact of skill development, of unlocking potential and instilling a mindset of excellence.
+/p>His foray into coaching was a natural evolution, driven by a desire to nurture talent and cultivate greatness. As he trained a group of local high school kids, he witnessed their transformation, their ascent to collegiate and even professional ranks. It was a realization of the profound impact of skill development, of unlocking potential and instilling a mindset of excellence.</p>
 
-But Timothy's coaching philosophy extends beyond the basics; it's about instilling versatility and adaptability. He believes in crafting not just players, but complete athletes, capable of excelling in any position on the court. With a focus on ball handling prowess, he earned the moniker "ChosenHandle," a testament to his legendary skills and innovative training methods.
+<p>But Timothy's coaching philosophy extends beyond the basics; it's about instilling versatility and adaptability. He believes in crafting not just players, but complete athletes, capable of excelling in any position on the court. With a focus on ball handling prowess, he earned the moniker "ChosenHandle," a testament to his legendary skills and innovative training methods.</p>
 
-As an exceptional basketball training coach, Timothy Prater embodies excellence and dedication. His unique blend of expertise and inspiration empowers athletes to transcend their limitations and reach new heights. With a strategic focus on fundamentals and tactical brilliance, he's become a beacon of guidance for aspiring ballers worldwide.
+<p>As an exceptional basketball training coach, Timothy Prater embodies excellence and dedication. His unique blend of expertise and inspiration empowers athletes to transcend their limitations and reach new heights. With a strategic focus on fundamentals and tactical brilliance, he's become a beacon of guidance for aspiring ballers worldwide.</p>
 
-Join Timothy Prater on a journey of transformation, where passion meets purpose, and greatness knows no bounds. Elevate your game with the #BasketballCoach #TrainingGuru who's redefining the art of basketball mastery.</p>
+<p>Join Timothy Prater on a journey of transformation, where passion meets purpose, and greatness knows no bounds. Elevate your game with the #BasketballCoach #TrainingGuru who's redefining the art of basketball mastery.</p>
             </article>
             <h3>Accomplishments</h3>
             <ul>
