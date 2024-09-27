e6d2ab4e86e50408c763e0974462ee4268ba281e
ajp1984 <prater.anthony.j@gmail.com>
2024-09-26 23:11:42 -0700

    Update NewsFeed.vue

--- src/components/NewsFeed.vue
+++ src/components/NewsFeed.vue
@@ -3,6 +3,7 @@
 </script>
 <template>
     <div class="container">
+        <h1>News from CEA</h1>
         <iframe src="https://www.youtube.com/embed/IeqNVFVaEOs?si=sBCByk6bUqgeoMGa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 </template>
@@ -14,6 +15,12 @@
     display: grid;
     place-items: center;
     overflow: auto;
+}
+
+h1,h2,h3 {
+  text-shadow: 1px 1px 1px #000;
+  padding: 1rem;
+  color: var(--green);
 }
 
 /* ----------- New Gallery Images ----------- */
